5ab20f819a4d2ceb3edb09e25fcf8da15921e09b
junha-ahn <junha.ahn.dev@gmail.com>
2020-03-16 22:40:27 +0900

    Express 설정

---
+++ src/config/index.js
@@ -0,0 +1,17 @@
+const path = require("path");
+global.$require = pathname => require(path.join(__dirname, "../" + pathname));
+
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) return val;
+  if (port >= 0) return port;
+  return false;
+};
+
+module.exports = {
+  NODE_ENV: process.env.NODE_ENV,
+  port: normalizePort(process.argv[2] || process.env.PORT || 3000),
+  logs: {
+    level: process.env.LOG_LEVEL || "silly"
+  },
+};
