610564724cebdeba0f37a31059fb4ede427e9a80
junha-ahn <junha.ahn.dev@gmail.com>
2020-03-18 18:56:20 +0900

    test https

--- src/config/index.js
+++ src/config/index.js
@@ -7,10 +7,12 @@ const normalizePort = val => {
   if (port >= 0) return port;
   return false;
 };
-
+const HTTPS_ENABLE = 1;
 module.exports = {
+  HTTPS_ENABLE,
   NODE_ENV: process.env.NODE_ENV,
-  port: normalizePort(process.argv[2] || process.env.PORT || 3000),
+  port: normalizePort(HTTPS_ENABLE ? 443 : 
+    process.argv[2] || process.env.PORT || 3000),
   logs: {
     level: process.env.LOG_LEVEL || "silly"
   },
--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
-const config = require('./config');
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const config = require('./config');
 const Logger = $require('loaders/logger');
 
 function startServer() {
   const app = express();
-  const server = require('http').createServer(app);
+  const server = !config.HTTPS_ENABLE ? 
+  require('http').createServer(app) :
+  require('https').createServer({
+    key: fs.readFileSync(path.join(__dirname, '../key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, '../cert.pem'))
+  }, app);
 
   $require('loaders')(app, server);
 
